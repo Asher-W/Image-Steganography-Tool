46e698e86eb74a37a712619cc305e369dd06f76a
Asher-W <85578685+Asher-W@users.noreply.github.com>
2021-07-09 20:50:29 -0700

    Still very glitchy and unfinished

--- DecodeSteganography.py
+++ DecodeSteganography.py
@@ -25,19 +25,14 @@ def getMessage():
             encoded.append(newPix)
         else:
             seed = ord(savedURL[-1]) + index * 2
-            possibleChange = 1
             while 1:
                 newPix = [getSudoRandom(seed * width, 0, width), getSudoRandom((seed + height/2) * height, 0, height)]
                 if newPix not in encoded: break
-                if newPix[0] < width-possibleChange: newPix[0] +=possibleChange
-                if newPix[1] < height-possibleChange: newPix[1] += possibleChange
-                if newPix not in encoded: break
-                seed += newPix[0] * 2 + possibleChange * 5
-                possibleChange += 1
+                seed += newPix[0] * 2 + 1
             encoded.append(newPix)
             savedURL = savedURL + chr((pixels[newPix[0],newPix[1]][0] - baseColor[0]) + (pixels[newPix[0],newPix[1]][1] - baseColor[1]) + 
               (pixels[newPix[0],newPix[1]][2] - baseColor[2]) + 64)
-    print(savedURL)
+            print(savedURL)
 
 def stringToNum(str : str):
     total = 0
@@ -47,21 +42,24 @@ def stringToNum(str : str):
 def getSudoRandom(seed, base, top):
     seed, base, top = int(seed), int(base), int(top)
     seedBin = bin(seed)[2:]
-    endbin = bin(top)[2:]
-    extraZeros = len(seedBin) - len(endbin)
+    endBin = bin(top)[2:]
+    extraZeros = len(seedBin) - len(endBin)
     if extraZeros > 0:
-        for i in range(0, extraZeros, 1): endbin = "0" + endbin
+        for i in range(0, extraZeros, 1): endBin = "0" + endBin
     if extraZeros < 0:
         for i in range(0, extraZeros, -1): seedBin = "0" + seedBin
     returnNum = ""
-    for i in range(len(returnNum)):
-        if returnNum[i] == endbin[i]: returnNum = returnNum + "0"
+    for i in range(len(endBin)):
+        if seedBin[i] == endBin[i]: returnNum = returnNum + "0"
         else: returnNum = returnNum + "1"
+    print(returnNum)
     num = 0
     for i, v in enumerate(returnNum):
-        num += i ** v
+        num += int(v) * 2 ** i
     returnNum = num
     del num
+
+    print(returnNum)
     return min(max((returnNum ** 3 + base) ** 2 % (top - base), base), top - 1)
 
 getMessage()
--- SteganographyEncoder.py
+++ SteganographyEncoder.py
@@ -52,18 +52,11 @@ def encode_message(fname, text, URL):
             encoded.append(newPix)
         else:
             seed = ord(URL[index - 1]) + index * 2
-            if index < 10: print(URL[index - 1], index, seed)
-            possibleChange = 1
             while 1:
                 newPix = [getSudoRandom(seed * width, 0, width), getSudoRandom((seed + height/2) * height, 0, height)]
                 if newPix not in encoded: break
-                if newPix[0] < width-possibleChange: newPix[0] +=possibleChange
-                if newPix[1] < height-possibleChange: newPix[1] += possibleChange
-                if newPix not in encoded: break
-                seed += newPix[0] * 2 + possibleChange * 5
-                possibleChange += 1
+                seed += newPix[0] * 2 + 1
             encoded.append(newPix)
-            if index < 10: print("    " + URL[:index + 1], newPix)
         baseRed += data[encoded[-1][0] + (encoded[-1][1] * width)][0]
         baseGreen += data[encoded[-1][0] + (encoded[-1][1] * width)][1]
         baseBlue += data[encoded[-1][0] + (encoded[-1][1] * width)][2]
@@ -105,22 +98,21 @@ def stringToNum(str : str):
 def getSudoRandom(seed, base, top):
     seed, base, top = int(seed), int(base), int(top)
     seedBin = bin(seed)[2:]
-    endbin = bin(top)[2:]
-    extraZeros = len(seedBin) - len(endbin)
+    endBin = bin(top)[2:]
+    extraZeros = len(seedBin) - len(endBin)
     if extraZeros > 0:
-        for i in range(0, extraZeros, 1): endbin = "0" + endbin
+        for i in range(0, extraZeros, 1): endBin = "0" + endBin
     if extraZeros < 0:
         for i in range(0, extraZeros, -1): seedBin = "0" + seedBin
     returnNum = ""
-    for i in range(len(returnNum)):
-        if returnNum[i] == endbin[i]: returnNum = returnNum + "0"
+    for i in range(len(endBin)):
+        if seedBin[i] == endBin[i]: returnNum = returnNum + "0"
         else: returnNum = returnNum + "1"
     num = 0
     for i, v in enumerate(returnNum):
-        num += i ** v
+        num += int(v) * 2 ** i
     returnNum = num
     del num
-
     return min(max((returnNum ** 3 + base) ** 2 % (top - base), base), top - 1)
 
 #create the object to hold widgets
